19da3fbca0a88a77a66e05680b42a118b9d53bfd
Oponjous <joe.ikdura@gmail.com>
2023-12-22 01:07:01 +0100

    committing state management with useReducer pratice

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import CartContextProvider from "./store/cart-context.jsx";
+function App() {
+  return (
+    <CartContextProvider>
+      <Header />
+      <Shop />
+    </CartContextProvider>
+  );
+}
+
+export default App;
